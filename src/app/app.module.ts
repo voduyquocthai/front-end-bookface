190cda4983b89d1f6d48d6ef0f52af6b1d7a9a4e
tranminhthong <minhthong9711@gmail.com>
2021-05-19 08:36:22 +0700

    add component update profile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UpdateProfileComponent } from './user/update-profile/update-profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UpdateProfileComponent
   ],
   imports: [
     BrowserModule,
