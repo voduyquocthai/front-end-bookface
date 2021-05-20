e9f8283217e97cb3be622321810262ed513d7a62
voduyquocthai <voduyquocthai@gmail.com>
2021-05-20 14:35:00 +0700

    fix css

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -17,12 +17,14 @@ export class HeaderComponent implements OnInit {
   ngOnInit() {
     this.authService.loggedIn.subscribe((data: boolean) => this.isLoggedIn = data);
     this.authService.username.subscribe((data: string) => this.username = data);
+    this.authService.userId.subscribe((data: number) => this.userId = data)
     this.isLoggedIn = this.authService.isLoggedIn();
     this.username = this.authService.getUserName();
+    this.userId = this.authService.getUserId();
   }
 
   goToUserProfile() {
-    this.router.navigateByUrl('/user-profile/' + this.username);
+    this.router.navigateByUrl('users/user-profile/' + this.userId);
   }
 
   logout() {
