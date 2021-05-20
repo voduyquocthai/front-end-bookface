f26f74f52c58eff15a6c7977ec523a5d44d2e0e9
voduyquocthai <voduyquocthai@gmail.com>
2021-05-21 01:41:35 +0700

    them giao dien UI home

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -2,7 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {environment} from '../../environments/environment';
 // @ts-ignore
-import {Post} from '../model/Post';
+import {Post} from '../model/post';
 import {Observable} from 'rxjs';
 
 const API_URL = `${environment.apiUrl}`;
