dd63ab2e907f8514dd775af6f1e92f53fdfc4985
tranminhthong <minhthong9711@gmail.com>
2021-05-23 04:18:17 +0700

    added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://book-face-be.herokuapp.com',
+  firebaseConfig: {
+    apiKey: 'AIzaSyAzjvclwX9lyH3u4OkS0FGSUVbTYQbL4Ac',
+    authDomain: 'fir-upload-7af9a.firebaseapp.com',
+    projectId: 'fir-upload-7af9a',
+    storageBucket: 'fir-upload-7af9a.appspot.com',
+    messagingSenderId: '841290005883',
+    appId: '1:841290005883:web:b1e1d757ca0a47165491de',
+    measurementId: 'G-85SL87VQBV'
+  },
+  apiBaseServer: 'https://book-face-be.herokuapp.com'
 };
