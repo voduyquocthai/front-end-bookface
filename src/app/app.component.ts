88ff9011d8fff09b19d57555b0281f96c57ef42f
cgc0220h1 <anhnam420@gmail.com>
2021-05-24 00:00:24 +0700

    Add abilities to load the login data from local storage as soon as the application loads

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from './auth/auth.service';
+import { StateService } from './services/state.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'front-end-bookface';
+
+  constructor(private authServie: AuthService,
+              private router: Router,
+              private stateService: StateService) {
+  }
+
+  /**
+   * Check the JWT Token in Local Storage everytime client have access to the site. This method
+   * is called once (if the user does not reload the website)
+   */
+  ngOnInit(): void {
+    if (this.authServie.getJwtToken() != null) {
+      this.stateService.userLoginObservable.next({
+        username: this.authServie.getUserName(),
+        userid: this.authServie.getUserId(),
+      })
+    } else {
+      this.router.navigateByUrl('/login')
+    }
+  }
+
 }
