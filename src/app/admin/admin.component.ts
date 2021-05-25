cd43e6a8fc0a9df3f834ec2807fc687a26268c7a
Nguyễn Khắc Qui <hivongmotloihua@gmail.com>
2021-05-25 23:22:20 +0700

    add

---
+++ src/app/admin/admin.component.ts
@@ -0,0 +1,41 @@
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../environments/environment';
+import {User} from '../user/user';
+import {UsersService} from '../user/service/users.service';
+
+const API_URL = `${environment.apiUrl}`;
+
+@Component({
+  selector: 'app-admin',
+  templateUrl: './admin.component.html',
+  styleUrls: ['./admin.component.css']
+})
+
+export class AdminComponent implements OnInit {
+
+  userActivated: User[];
+  userBlocked: User[];
+
+  constructor(private userService: UsersService) {
+  }
+
+  ngOnInit(): void {
+    this.getAllUserActivated();
+
+  }
+
+  getAllUserActivated() {
+    return this.userService.getAllUserActivated().subscribe(value => {
+      this.userActivated = value;
+      console.log(this.userActivated);
+    });
+  }
+
+  getAllUserBlocked() {
+    return this.userService.getAllUserBlocked().subscribe(value => {
+      this.userBlocked = value;
+      console.log(this.userBlocked);
+    });
+  }
+}
