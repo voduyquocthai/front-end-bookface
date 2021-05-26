1c7f724ffc2f9e00cf70c9bd3d366790424bc589
MInhHien93 <nguyminhhien2593@gmail.com>
2021-05-26 18:10:37 +0700

    push created comment to comments

--- src/app/shared/comment/comment.component.ts
+++ src/app/shared/comment/comment.component.ts
@@ -6,6 +6,7 @@ import {CommentService} from "../../services/comment.service";
 import {AuthService} from "../../auth/auth.service";
 import {UsersService} from "../../user/service/users.service";
 import {PostModel} from "../../post/post-model";
+import {Router} from "@angular/router";
 
 
 @Component({
@@ -15,16 +16,17 @@ import {PostModel} from "../../post/post-model";
 })
 export class CommentComponent implements OnInit {
   currentUser: User;
+  commentModel: CommentModel = {};
+  comments: CommentModel[] = [];
   commentForm: FormGroup = new FormGroup({
     text: new FormControl()
   });
   @Input() post: PostModel;
-  commentModel: CommentModel = {};
-  comments: CommentModel[] = [];
 
   constructor(private commentService: CommentService,
               private authService: AuthService,
-              private userService: UsersService) {
+              private userService: UsersService,
+              private router: Router) {
   }
 
   ngOnInit(): void {
@@ -36,9 +38,11 @@ export class CommentComponent implements OnInit {
     this.commentModel.text = this.commentForm.get('text').value;
     this.commentModel.postId = this.post.id;
     this.commentService.createComment(this.commentModel).subscribe(
-      ()=>{console.log('Created comment');},
+      data =>{console.log('Created comment');
+        this.comments.push(data)},
     (e) => {console.log(e.message)}
     )
+    this.commentForm.reset();
   }
 
   getCurrentUser() {
@@ -56,4 +60,7 @@ export class CommentComponent implements OnInit {
   }
 
 
+  goToUserProfile(userId: number) {
+    this.router.navigateByUrl('users/user-profile/' + userId);
+  }
 }
