832ae07c686a5586ab9de3c93c5057c48c8ca120
MInhHien93 <nguyminhhien2593@gmail.com>
2021-05-27 14:02:20 +0700

    can't update or delete

--- src/app/shared/comment/comment.component.ts
+++ src/app/shared/comment/comment.component.ts
@@ -1,12 +1,13 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {User} from "../../user/user";
-import {FormControl, FormGroup} from "@angular/forms";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {CommentModel} from "../../model/component/comment-model";
 import {CommentService} from "../../services/comment.service";
 import {AuthService} from "../../auth/auth.service";
 import {UsersService} from "../../user/service/users.service";
 import {PostModel} from "../../post/post-model";
 import {Router} from "@angular/router";
+import {HttpErrorResponse} from "@angular/common/http";
 
 
 @Component({
@@ -18,9 +19,18 @@ export class CommentComponent implements OnInit {
   currentUser: User;
   commentModel: CommentModel = {};
   comments: CommentModel[] = [];
-  commentForm: FormGroup = new FormGroup({
+  addCommentForm: FormGroup = new FormGroup({
     text: new FormControl()
   });
+  editCommentForm: FormGroup = new FormGroup({
+    id: new FormControl(),
+    text: new FormControl(),
+    likeCount: new FormControl(),
+    heartCount: new FormControl()
+  });
+  editComment: CommentModel = {};
+  deletedComment: CommentModel = {};
+
   @Input() post: PostModel;
 
   constructor(private commentService: CommentService,
@@ -35,14 +45,18 @@ export class CommentComponent implements OnInit {
   }
 
   createComment() {
-    this.commentModel.text = this.commentForm.get('text').value;
+    this.commentModel.text = this.addCommentForm.get('text').value;
     this.commentModel.postId = this.post.id;
     this.commentService.createComment(this.commentModel).subscribe(
-      data =>{console.log('Created comment');
-        this.comments.push(data)},
-    (e) => {console.log(e.message)}
+      data => {
+        console.log('Created comment');
+        this.getAllCommentForPost();
+      },
+      (e) => {
+        console.log(e.message)
+      }
     )
-    this.commentForm.reset();
+    this.addCommentForm.reset();
   }
 
   getCurrentUser() {
@@ -54,13 +68,62 @@ export class CommentComponent implements OnInit {
 
 
   getAllCommentForPost() {
-    this.commentService.getAllCommentForPost(this.post.id).subscribe(comments =>{
-      this.comments =comments;
+    this.commentService.getAllCommentForPost(this.post.id).subscribe(comments => {
+      this.comments = comments;
     })
   }
 
 
   goToUserProfile(userId: number) {
     this.router.navigateByUrl('users/user-profile/' + userId);
+  }
+
+  openModal(mode: string, comment: CommentModel) {
+    const container = document.getElementById('main-container');
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-bs-toggle', 'modal');
+    console.log(comment);
+    if (mode === 'edit') {
+      this.editComment = comment;
+      console.log(this.editComment);
+      button.setAttribute('data-bs-target', '#editCommentModal');
+    }
+    if (mode === 'delete') {
+      this.deletedComment = comment;
+      button.setAttribute('data-bs-target', '#deleteCommentModal');
+    }
+    container.appendChild(button);
+    button.click();
+  }
+
+  updateComment() {
+    this.editComment.id =this.editCommentForm.get('id').value;
+    this.editComment.text =this.editCommentForm.get('text').value;
+    this.editComment.likeCount =this.editCommentForm.get('likeCount').value;
+    this.editComment.heartCount =this.editCommentForm.get('heartCount').value;
+    this.commentService.updateComment(this.editComment).subscribe(
+      response => {
+        console.log(response);
+        document.getElementById("close-edit-btn").click();
+        this.getAllCommentForPost();
+      },
+      (error: HttpErrorResponse) => {
+        console.log(error.message);
+      }
+    );
+  }
+
+  deleteComment() {
+    this.commentService.deleteComment(this.deletedComment.id).subscribe(
+      () => {
+        document.getElementById("close-delete-btn").click();
+        this.getAllCommentForPost();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
   }
 }
