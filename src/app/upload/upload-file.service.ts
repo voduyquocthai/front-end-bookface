7ea3045d63ed1d84a8ad20b73733862ef8e8b39e
voduyquocthai <voduyquocthai@gmail.com>
2021-05-31 00:50:45 +0700

    done cơ bản chat socket

--- src/app/upload/upload-file.service.ts
+++ src/app/upload/upload-file.service.ts
@@ -28,13 +28,6 @@ export class UploadFileService {
     if (this.selectedImage !== null) {
       const filePath = `avatar/${this.selectedImage.name.split('.').slice(0, -1).join('.')}_${new Date().getTime()}`;
       const fileRef = this.storage.ref(filePath);
-      // this.storage.upload(filePath, this.selectedImage).snapshotChanges().pipe(
-      //   finalize(() => {
-      //     fileRef.getDownloadURL().subscribe(url => {
-      //       this.imgSrc = url;
-      //     });
-      //   })
-      // ).subscribe();
       return new Promise<any>((resolve, reject) => {
         const task = this.storage.upload(filePath, this.selectedImage);
         task.snapshotChanges().pipe(
